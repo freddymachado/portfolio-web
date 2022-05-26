d9bb3712b1b6e1d7364c138253fc11e488bbcb45
freddymachado <freddy_machado2@hotmail.com>
2022-05-26 15:09:00 -0400

    add preliminar pages

---
+++ pages/_documen.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+          <title>Personal Portfolio of Freddy Machado Developer</title>
+          <meta name="description" content='Freddy Machado Portfolio website'/>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
