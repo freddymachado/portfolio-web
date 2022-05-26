54fcf1a87676bb95a16368e8c5a948350d73d57c
freddymachado <freddy_machado2@hotmail.com>
2022-05-26 16:59:13 -0400

    add sample data

---
+++ profile.js
@@ -0,0 +1,60 @@
+export const skills = [
+    {
+        skill: 'Javascript',
+        percentage: 100,
+    },
+    {
+        skill: 'Nodejs',
+        percentage: 60,
+    },
+    {
+        skill: 'Physics',
+        percentage: 100,
+    },
+    {
+        skill: 'Economics',
+        percentage: 70,
+    },
+    {
+        skill: 'Databases',
+        percentage: 90,
+    },
+    {
+        skill: 'Java',
+        percentage: 100,
+    },
+    {
+        skill: 'Kotlin',
+        percentage: 80,
+    },
+    {
+        skill: 'Javascript',
+        percentage: 100,
+    },
+    {
+        skill: 'Javascript',
+        percentage: 100,
+    },
+    {
+        skill: 'Javascript',
+        percentage: 100,
+    },
+]
+
+export const projects = [
+    {
+        name: 'Awesome website 1',
+        description: 'Lorem ipsum',
+        image: 'portfolio1.jpg'
+    },
+    {
+        name: 'Awesome website 1',
+        description: 'Lorem ipsum',
+        image: 'portfolio1.jpg'
+    },
+    {
+        name: 'Awesome website 1',
+        description: 'Lorem ipsum',
+        image: 'portfolio1.jpg'
+    },
+]
