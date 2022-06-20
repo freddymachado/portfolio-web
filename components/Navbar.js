d032b459a7f9cdfdadc5a4300b6fb6fcd3140b2f
freddymachado <freddyantoniomachado@gmail.com>
2022-06-19 20:13:58 -0400

    fix navbar toggler bug

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,11 +15,13 @@ const Navbar = () => {
         <Link href="/">
           <a className="navbar-brand">Portfolio</a>    
         </Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" 
+        aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
                 <Link href="/">
                     <a className={activeClass("/")} aria-current="page">Inicio</a>
@@ -37,6 +39,7 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+        
       </div>
     </nav>
 
